6aca4500ec2746ed8f26ef17feaed718748288eb
Coleman <jackson@gariety.xxx>
2016-11-16 00:14:28 -0800

    Double rules work now!

--- Logic.hs
+++ Logic.hs
@@ -1,26 +1,68 @@
 module Logic where
 
-data Expr = Nil | NM String | Not Expr | And Expr Expr
+data Expr = Nil | NM String | Not Expr | And Expr Expr | Or Expr Expr | If Expr Expr | Iff Expr Expr | A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z
           deriving (Show, Eq)
 
 type Rule = Expr -> Maybe Expr
+type DoubleRule = Expr -> Expr -> Maybe Expr
 
 type Line = (Expr, (String, Expr))
 
+-- Double Negation Elimination
 dne :: Rule
 dne (Not (Not p)) = Just p
 dne _ = Nothing
 
+-- Double Negation Introduction
 dni :: Rule
 dni = Just . Not . Not
 
+-- Left And Elimination
 ael :: Rule
 ael (And p q) = Just p
 ael _ = Nothing
 
+-- Right And Elimination
 aer :: Rule
 aer (And p q) = Just q
 aer _ = Nothing
 
-rulePairs :: [(Rule, String)]
-rulePairs = [(dne, "DNE"), (dni, "DNI"), (ael, "AND ELIM"), (aer, "AND ELIM")]
+-- Left If and Only If Elimination
+iffel :: Rule
+iffel (Iff p q) = Just (If p q)
+iffel _ = Nothing
+
+-- Right If and Only If Elimination
+iffer :: Rule
+iffer (Iff p q) = Just (If q p)
+iffer _ = Nothing
+
+-- Modus Ponendo Ponens
+mpp :: DoubleRule
+mpp (If p q) r = if p == r then Just q else Nothing
+mpp _ _ = Nothing
+
+-- And Introduction
+ai :: DoubleRule
+ai p q = Just (And p q)
+
+-- Modus Tonendo Tollens
+mtt :: DoubleRule
+mtt (If p q) (Not r) = if q == r then Just (Not p) else Nothing
+mtt _ _ = Nothing
+
+-- Left Disjunctive Syllogism
+dsl :: DoubleRule
+dsl (Or p q) (Not r) = if p == r then Just q else Nothing
+dsl _ _ = Nothing
+
+-- Right Disjunctive Syllogism
+dsr :: DoubleRule
+dsr (Or p q) (Not r) = if q == r then Just p else Nothing
+dsr _ _ = Nothing
+
+rules :: [Rule]
+rules = [dne, dni, ael, aer, iffel, iffer]
+
+doubleRules :: [DoubleRule]
+doubleRules = [mpp, ai, mtt]
--- Main.hs
+++ Main.hs
@@ -1,36 +1,41 @@
+import Debug.Trace
 import Control.Monad
 import Data.List
 import Data.Maybe
-
 import Logic
 import Util
 
-apply :: [(Expr, t)] -> [Line]
-apply props = catMaybeFst [(r p, (n, p)) | pp <- props, rp <- rulePairs,
-                           let (r, n) = rp,
-                           let (p, o) = pp]
+applySingle :: [Expr] -> [Expr]
+applySingle props = catMaybes [r p | p <- props, r <- rules]
 
-resolve :: [Line] -> [[Line]]
+applyDouble :: [Expr] -> [Expr]
+applyDouble props = catMaybes [r p q | p <- props, q <- props, r <- doubleRules]
+
+apply :: [Expr] -> [Expr]
+apply props = nub (props ++ applySingle props ++ applyDouble props)
+
+resolve :: [Expr] -> [[Expr]]
 resolve = unfoldr (Just . join (,) . apply)
 
-addSups = map (\p -> (p, ("S", p)))
+isValid :: [Expr] -> Expr -> Bool
+isValid ps c = elem c . concat $ resolve ps
 
-prettify = map (\p -> (p, "S"))
-pretty (a, (b, c)) = (a, b)
+-- addSups = map (\p -> (p, ("S", p)))
 
-isValid ps c = elem c . map fst . concat . resolve . addSups $ ps
-
-prove ps conc = go conc []
-  where go seed res = if elem (prev line) ps
-                      then (prettify ps) ++ ((pretty line) : res)
-                      else go (prev line) ((pretty line) : res)
-          where line = findLine seed
-                findLine p = val . find (\b -> (prem b) == p) . concat $ rose
-                findConclusion b = all (not . (\a -> (prem a) == conc)) b
-                rose = takeWhileInclusive findConclusion (resolve (addSups ps))
+-- prove ps conc = go conc []
+--   where go seed res = if elem (prev line) ps
+--                       then (prettify ps) ++ ((pretty line) : res)
+--                       else go (prev line) ((pretty line) : res)
+--           where line = findLine seed
+--                 findLine p = val . find (\b -> (prem b) == p) . concat $ rose
+--                 findConclusion b = all (not . (\a -> (prem a) == conc)) b
+--                 rose = takeWhileInclusive findConclusion (resolve (addSups ps))
+--                 prettify = map (\p -> (p, "S"))
+--                 pretty (a, (b, c)) = (a, b)
 
 main :: IO ()
-main = mapM_ print $ prove
-       [
-         (Not (Not (And (NM "a") (NM "b"))))
-       ] (NM "b")
+main = print $ isValid
+
+       [ (Not (Not (If A B))),
+         (And (Not (Not (Not B))) (Not C)) 
+       ] (Not A)
